7d4cc2a990cdce9ccd539b4da2f370d00ea9d90f
Heinner Zapata <heinnerzapata1991@gmail.com>
2019-08-31 11:36:26 -0500

    Updates

---
+++ precache-manifest.84c1749752bacf15663b8da764ea02f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d195f6d1608ac0eb01ba61a08bff9cc",
+    "url": "/travel-risk/index.html"
+  },
+  {
+    "revision": "ec0ab22b29f89ad051a9",
+    "url": "/travel-risk/static/css/main.0a9e3bf5.chunk.css"
+  },
+  {
+    "revision": "589e721a1766c208ce2f",
+    "url": "/travel-risk/static/js/2.205747f1.chunk.js"
+  },
+  {
+    "revision": "ec0ab22b29f89ad051a9",
+    "url": "/travel-risk/static/js/main.55a4b98b.chunk.js"
+  },
+  {
+    "revision": "f3e4507f45a73fd8028b",
+    "url": "/travel-risk/static/js/runtime~main.0431558d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travel-risk/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/TD-React-Challenge-Test-1/precache-manifest.14c61b6dadeb3d030d0b3f77a9e9f401.js"
+  "/travel-risk/precache-manifest.84c1749752bacf15663b8da764ea02f8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TD-React-Challenge-Test-1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/travel-risk/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
